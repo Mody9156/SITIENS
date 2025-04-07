7af1a6456a75616deb5041b81a236bb8b473e970
KEITA Modibo <Modykeita2@gmail.com>
2025-04-07 20:01:18 +0200

    suppression du test et de l'image

---
+++ SITIENS/View/TapView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SITIENS
+//
+//  Created by Modibo on 07/04/2025.
+//
+
+import SwiftUI
+
+struct TapView: View {
+    var body: some View {
+        VStack {
+            TapView{
+                
+            }
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TapView()
+}
