011ebc492db5c778bf530a33c02b2b78dab52746
KEITA Modibo <Modykeita2@gmail.com>
2025-04-08 17:58:47 +0200

    modification de l'interface avec du text

--- SITIENS/View/HomeView.swift
+++ SITIENS/View/HomeView.swift
@@ -10,37 +10,30 @@ import SwiftUI
 struct HomeView: View {
     @State var slider : Double = 0
     var body: some View {
-        ZStack {
-            Color
-                .gray
-                .opacity(0.3)
+        ZStack{
+            Color("BackgroundColor")
                 .ignoresSafeArea()
             
-            ScrollView {
-                VStack {
-                    VStack {
-                        Image("thirstyPicture")
-                            .resizable()
-                            .frame(width: .infinity,height: 500)
-                            .scaledToFit()
-                            
-                        VStack {
-                            Text("Combien d’eau faut-il boire en moyenne chaque jour ?")
-                                .font(.title2)
-                                .foregroundStyle(.blue)
-                                .padding()
-                            
-                            Text("Notre organisme est composé de 60 à 65 % d’eau. Et c’est précisément cette eau présente dans notre corps qui permet d’assurer de nombreuses fonctions vitales de notre corps. On comprend alors pourquoi il est si important de boire régulièrement et en quantité suffisante pour maintenir notre corps en bonne santé.")
-                                .font(.callout)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .frame(width: 300)
-                        }
-                        
-                        
-                       
-                    }
-                    
-                }
+            VStack {
+                Spacer()
+                
+                Image(systemName: "waterbottle.fill")
+                    .resizable()
+                    .frame(width: 100,height: 200)
+                    .foregroundStyle(.white)
+                
+                Text("Combien d’eau faut-il boire en moyenne chaque jour ?")
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                    .padding()
+                
+                Text("Notre organisme est composé de 60 à 65 % d’eau. Et c’est précisément cette eau présente dans notre corps qui permet d’assurer de nombreuses fonctions vitales de notre corps. On comprend alors pourquoi il est si important de boire régulièrement et en quantité suffisante pour maintenir notre corps en bonne santé.")
+                    .font(.callout)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(width: 300)
+                
+                Spacer()
+                
             }
         }
     }
--- SITIENS/View/InfosView.swift
+++ SITIENS/View/InfosView.swift
@@ -9,10 +9,31 @@ import SwiftUI
 
 struct InfosView: View {
     var body: some View {
-        ZStack {
-            Color.blue.ignoresSafeArea()
+        ZStack{
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            
             VStack {
-                Text("Hello, World!")
+                
+                Spacer()
+                
+                Image(systemName: "waterbottle.fill")
+                    .resizable()
+                    .frame(width: 100,height: 200)
+                    .foregroundStyle(.white)
+                
+                Text("Boire de l’eau : quelle est la limite à ne pas dépasser ?")
+                    .font(.title2)
+                    .foregroundStyle(.white)
+                    .padding()
+                
+                Text("D’une manière générale, il est fortement déconseillé de boire plus de 5 litres d’eau par jour, car une grande quantité d’eau risque de diluer les constantes sanguines. Voyons quelles peuvent être les différentes conséquences d’une surconsommation d’eau.")
+                    .font(.callout)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(width: 300)
+                
+                Spacer()
+                
             }
         }
     }
