518cc6d81cab911679393d4169d7895614f8a96a
KEITA Modibo <Modykeita2@gmail.com>
2025-04-08 19:23:04 +0200

    ajout de navigationStack

--- SITIENS/View/InfosView.swift
+++ SITIENS/View/InfosView.swift
@@ -9,31 +9,38 @@ import SwiftUI
 
 struct InfosView: View {
     var body: some View {
-        ZStack{
-            Color("BackgroundColor")
-                .ignoresSafeArea()
-            
-            VStack {
+        NavigationStack {
+            ZStack{
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
                 
-                Spacer()
-                
-                Image(systemName: "drop")
-                    .resizable()
-                    .frame(width: 100,height: 200)
-                    .foregroundStyle(.white)
-                
-                Text("Boire de l’eau : quelle est la limite à ne pas dépasser ?")
-                    .font(.title2)
-                    .foregroundStyle(.white)
-                    .padding()
-                
-                Text("D’une manière générale, il est fortement déconseillé de boire plus de 5 litres d’eau par jour, car une grande quantité d’eau risque de diluer les constantes sanguines. Voyons quelles peuvent être les différentes conséquences d’une surconsommation d’eau.")
-                    .font(.callout)
-                    .fixedSize(horizontal: false, vertical: true)
-                    .frame(width: 300)
-                
-                Spacer()
-                
+                VStack {
+                    
+                    Spacer()
+                    
+                    Image(systemName: "drop")
+                        .resizable()
+                        .frame(width: 100,height: 200)
+                        .foregroundStyle(.white)
+                    
+                    Text("Boire de l’eau : quelle est la limite à ne pas dépasser ?")
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                        .padding()
+                    
+                    Text("D’une manière générale, il est fortement déconseillé de boire plus de 5 litres d’eau par jour, car une grande quantité d’eau risque de diluer les constantes sanguines. Voyons quelles peuvent être les différentes conséquences d’une surconsommation d’eau.")
+                        .font(.callout)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(width: 300)
+                    
+                    Spacer()
+                    
+                    NavigationLink {
+                        Timer()
+                    } label: {
+                    Image(systemName: "rectangle.portrait.and.arrow.right.fill")
+                    }
+                }
             }
         }
     }
