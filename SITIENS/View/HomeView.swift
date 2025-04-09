f5ea6bc448da5170e83233c6613f924a2502d636
KEITA Modibo <Modykeita2@gmail.com>
2025-04-09 16:23:17 +0200

    nouveau APPstorage

--- SITIENS/View/HomeView.swift
+++ SITIENS/View/HomeView.swift
@@ -8,32 +8,59 @@
 import SwiftUI
 
 struct HomeView: View {
+    @AppStorage("navlink") var activeNavLink : Bool = false
+    
     var body: some View {
-        ZStack{
-            Color("BackgroundColor")
-                .ignoresSafeArea()
-            
-            VStack {
+        NavigationStack {
+            ZStack{
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
                 
-                Spacer()
-                
-                Image(systemName: "waterbottle.fill")
-                    .resizable()
-                    .frame(width: 100,height: 200)
-                    .foregroundStyle(.white)
-                
-                Text("Combien d’eau faut-il boire en moyenne chaque jour ?")
-                    .font(.title2)
-                    .foregroundStyle(.white)
+                VStack {
+                    
+                    HStack {
+                        
+                        Spacer()
+                        NavigationLink {
+                            Timer()
+                        } label: {
+                            Button(action:{
+                                activeNavLink = true
+                            }){
+                                Text("Skip")
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                            }
+                        }
+
+                     
+                        
+                    }
                     .padding()
-                
-                Text("Notre organisme est composé de 60 à 65 % d’eau. Et c’est précisément cette eau présente dans notre corps qui permet d’assurer de nombreuses fonctions vitales de notre corps. On comprend alors pourquoi il est si important de boire régulièrement et en quantité suffisante pour maintenir notre corps en bonne santé.")
-                    .font(.callout)
-                    .fixedSize(horizontal: false, vertical: true)
-                    .frame(width: 300)
-                
-                Spacer()
-                
+                    
+                    Spacer()
+                    
+                    Image(systemName: "waterbottle.fill")
+                        .resizable()
+                        .frame(width: 100,height: 200)
+                        .foregroundStyle(.white)
+                    
+                    Text("Combien d’eau faut-il boire en moyenne chaque jour ?")
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                        .padding()
+                    
+                    Text("Notre organisme est composé de 60 à 65 % d’eau. Et c’est précisément cette eau présente dans notre corps qui permet d’assurer de nombreuses fonctions vitales de notre corps. On comprend alors pourquoi il est si important de boire régulièrement et en quantité suffisante pour maintenir notre corps en bonne santé.")
+                        .font(.callout)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(width: 300)
+                    
+                    Spacer()
+                    
+                    
+                    
+                    
+                }
             }
         }
     }
