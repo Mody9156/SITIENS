1eee9a6073305ddd1307f1fb941c5d5da76470a5
KEITA Modibo <Modykeita2@gmail.com>
2025-04-09 17:47:13 +0200

    Ajout d'un ViewModel

---
+++ SITIENS/ViewModel/hydrationActivationViewModel .swift
@@ -0,0 +1,13 @@
+//
+//  hydrationActivationViewModel .swift
+//  SITIENS
+//
+//  Created by Modibo on 09/04/2025.
+//
+
+import Foundation
+
+
+class hydrationActivationViewModel : ObservableObject {
+    
+}
