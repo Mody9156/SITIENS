c22578855c20c1ce9b553f034a6e5282d727d20b
KEITA Modibo <Modykeita2@gmail.com>
2025-04-09 18:44:35 +0200

    mise en place d'une fonction

--- SITIENS/ViewModel/hydrationActivationViewModel .swift
+++ SITIENS/ViewModel/hydrationActivationViewModel .swift
@@ -6,8 +6,38 @@
 //
 
 import Foundation
+import UserNotifications
 
 
 class hydrationActivationViewModel : ObservableObject {
     
+    func notification(){
+        //Contenu de la notification
+        let userNotification = UNMutableNotificationContent()
+        userNotification.title = "Il est temps de boire de l'eau"
+        userNotification.sound = UNNotificationSound.default
+        userNotification.body = "Boire de l'eau est essentiel à notre bien-être"
+        
+        //Date d'activation
+        var dateComponents = DateComponents()
+        dateComponents.hour = 0
+
+        
+        
+        //Répéter la notification
+        for hour in stride(from: 8, to: 20, by: 1){
+            dateComponents.hour = hour
+            
+            //Trigger (déclanche quand la notification sera envoyée)
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            //Request
+            let request = UNNotificationRequest(identifier: "hydradation \(hour)", content: userNotification, trigger: trigger)
+            UNUserNotificationCenter.current().add(request)
+
+        }
+        
+        
+    }
+    
 }
+
