c36f6a6e954188557a1b0e95610c5de1d5143b59
KEITA Modibo <Modykeita2@gmail.com>
2025-04-17 19:01:19 +0200

    nouveau navStack

--- SITIENS/View/WaterQuantityView.swift
+++ SITIENS/View/WaterQuantityView.swift
@@ -12,55 +12,57 @@ struct WaterQuantityView: View {
     @State var title : String = "150ml"
     @State var nameOfCategory : String = "Nourrisson"
     var body: some View {
-        VStack{
-            
-            Text("\(Int((updateHeight / 2) * 200 / 300)) %")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(Color.blue)
-                .padding()
-            
-            
-            Text("\(Int(updateHeight / 2))ml / \(title)")
-                .foregroundStyle(.gray)
-            
-            ZStack (alignment: .bottom){
-                RoundedRectangle(cornerRadius: 6)
-                    .frame(width: 200,height: 300)
-                    .foregroundStyle(.white)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.blue, lineWidth: 2)
-                    }
+        NavigationStack {
+            VStack{
                 
-                RoundedRectangle(cornerRadius: 6)
-                    .frame(width: 200,height: updateHeight)
-                    .foregroundStyle(.blue)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 6)
-                            .foregroundStyle(Color.blue)
-                    }
-            }
-            .padding()
-            
-            Button {
-                withAnimation {
-                    if updateHeight != 300 {
-                        updateHeight += 12.5
-                    }
-                }
+                Text("\(Int((updateHeight / 2) * 200 / 300)) %")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.blue)
+                    .padding()
                 
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(width: 200,height: 50)
-                    Text(updateHeight == 300 ? "Objectif atteint" : "Ajouter de l'eau")
+                
+                Text("\(Int(updateHeight / 2))ml / \(title)")
+                    .foregroundStyle(.gray)
+                
+                ZStack (alignment: .bottom){
+                    RoundedRectangle(cornerRadius: 6)
+                        .frame(width: 200,height: 300)
                         .foregroundStyle(.white)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.blue, lineWidth: 2)
+                        }
+                    
+                    RoundedRectangle(cornerRadius: 6)
+                        .frame(width: 200,height: updateHeight)
+                        .foregroundStyle(.blue)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 6)
+                                .foregroundStyle(Color.blue)
+                        }
                 }
+                .padding()
+                
+                Button {
+                    withAnimation {
+                        if updateHeight != 300 {
+                            updateHeight += 12.5
+                        }
+                    }
+                    
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .frame(width: 200,height: 50)
+                        Text(updateHeight == 300 ? "Objectif atteint" : "Ajouter de l'eau")
+                            .foregroundStyle(.white)
+                    }
+                }
+                .padding()
+                
+                
             }
-            .padding()
-            
-            
         }
     }
 }
