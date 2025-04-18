bedae5baf4ab79c1f483d565aebfd8b4c9426513
KEITA Modibo <Modykeita2@gmail.com>
2025-04-18 15:21:10 +0200

    nouvelel list

--- SITIENS/View/UserSettingsView.swift
+++ SITIENS/View/UserSettingsView.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct UserSettingsView: View {
     @State var profileType : [String] = ["nourrissons","femmes enceintes", "personnes âgées","sportifs"]
+    @State var selectedProfileType : String = "nourrissons"
     
     var body: some View {
         VStack {
+            List {
+                Picker("Profile", selection: $selectedProfileType) {
+                    ForEach(profileType,id: \.self) { profile in
+                        Text(profile)
+                    }
+                }
+                .pickerStyle(.inline)
+            }
+            
             Text("Profil")
             
         }
