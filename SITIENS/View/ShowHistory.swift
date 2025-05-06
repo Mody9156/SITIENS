a645dcc63746851e81e22fba05ba01d14039044f
KEITA Modibo <Modykeita2@gmail.com>
2025-05-06 17:38:50 +0200

    nouvelel variable

--- SITIENS/View/ShowHistory.swift
+++ SITIENS/View/ShowHistory.swift
@@ -9,39 +9,47 @@ import SwiftUI
 
 struct ShowHistory: View {
     @Binding var historyManager : [HistoryManager]
-    
+    var dateformatted = Date.now.formatted(date: .numeric, time: .shortened)
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            
-            ForEach(historyManager) { historyManager in
-                if historyManager.name.isEmpty{
-                    Text("Vide")
-                        .font(.caption)
-                }else{
-                    HStack(spacing: 12){
-                        Text(historyManager.name)
-                            .foregroundStyle(.blue)
-                            .font(.callout)
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 12)
-                                .frame(width: 80, height: 30)
+        ScrollView {
+            VStack(alignment: .center, spacing: 8) {
+                Text("Historique")
+                    .font(.system(size: 40, weight: .light, design: .serif))
+                    .foregroundStyle(.gray)
+                    .padding()
+                
+                ForEach(historyManager) { historyManager in
+                    if historyManager.name.isEmpty{
+                        Text("Vide")
+                            .font(.caption)
+                    }else{
+                        Text("\(dateformatted)")
+                        HStack(spacing: 12){
+                            Text("\(historyManager.name) :")
                                 .foregroundStyle(.blue)
-                            Text("\(historyManager.quantity)ml")
-                                .foregroundStyle(.white)
+                                .font(.callout)
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .frame(width: 80, height: 30)
+                                    .foregroundStyle(.blue)
+                                Text("\(historyManager.quantity)ml")
+                                    .foregroundStyle(.white)
+                            }
+                            Image(systemName: "drop.degreesign")
+                                .foregroundStyle(.blue)
                         }
-                        Image(systemName: "drop.degreesign.fill")
-                            .foregroundStyle(.blue)
+                        
                     }
+                    
+                    
                 }
-                
-                
             }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
-    @Previewable @State var historyManager : [HistoryManager] = [HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00")]
+    @Previewable @State var historyManager : [HistoryManager] = [HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Jeune", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "4.00"),HistoryManager(name: "Sportif", quantity: "1.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Agée", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "3.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Enfant", quantity: "1.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Femme", quantity: "2.00"),HistoryManager(name: "Sportif", quantity: "0.70"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Homme", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "3.00"),HistoryManager(name: "Sportif", quantity: "11.00"),HistoryManager(name: "Sportif", quantity: "11.00")]
     ShowHistory(historyManager:$historyManager )
 }
