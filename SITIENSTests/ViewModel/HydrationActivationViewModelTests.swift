e6686e78ede5d1d74cf5b658c906440ed4be2962
KEITA Modibo <Modykeita2@gmail.com>
2025-05-14 17:56:52 +0200

    nouveau fichier

---
+++ SITIENSTests/ViewModel/HydrationActivationViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  HydrationActivationViewModelTests.swift
+//  SITIENSTests
+//
+//  Created by Modibo on 14/05/2025.
+//
+
+import Testing
+
+struct HydrationActivationViewModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
