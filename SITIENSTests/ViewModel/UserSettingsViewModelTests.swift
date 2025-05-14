95bb7a1413795cf904fd2c6ebf65e2152c47dc2b
KEITA Modibo <Modykeita2@gmail.com>
2025-05-14 17:23:45 +0200

    nouveau fichier

---
+++ SITIENSTests/ViewModel/UserSettingsViewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettingsViewModelTests.swift
+//  SITIENSTests
+//
+//  Created by Modibo on 14/05/2025.
+//
+
+import Testing
+
+struct UserSettingsViewModelTests {
+
+    @Test func updateTypeWithNewName() async throws {
+        
+        
+       
+    }
+
+}
