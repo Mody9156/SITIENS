29681b4b9ee99463a9701eceb24651a741752376
KEITA Modibo <Modykeita2@gmail.com>
2025-05-19 13:29:12 +0200

    suppression des textes

--- SITIENS/View/ConfiTimer.swift
+++ SITIENS/View/ConfiTimer.swift
@@ -118,13 +118,6 @@ struct ConfiTimer: View {
                     
                 }
             } label: {
-//                Text("Valider")
-//                    .font(.headline)
-//                    .frame(maxWidth: .infinity)
-//                    .padding()
-//                    .foregroundStyle(.white)
-//                    .background(.blue)
-//                    .cornerRadius(16)
                 ZStack {
                     RoundedRectangle(cornerRadius: 12)
                         .frame(height: 50)
