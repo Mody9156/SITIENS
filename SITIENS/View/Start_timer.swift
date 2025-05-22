a48c6e5b9685136f54b3333266541f10fe609774
KEITA Modibo <Modykeita2@gmail.com>
2025-05-22 17:30:54 +0200

    nouvelle vue

---
+++ SITIENS/View/Start_timer.swift
@@ -0,0 +1,136 @@
+//
+//  Start_timer.swift
+//  SITIENS
+//
+//  Created by Modibo on 22/05/2025.
+//
+
+import SwiftUI
+import Combine
+
+struct Start_timer: View {
+    @Binding var showMessage : Bool
+    @Bindable var hydrationActivationViewModel = HydrationActivationViewModel()
+    @Binding var timeInterval: Int
+    @Binding var timerIsReading : Bool
+    @AppStorage("hour",store: .standard) var timerhour : Int = 0
+    @Binding private var cancellable: Cancellable?
+    @Binding var startDate: Date?
+    @Binding var elapseBeforPause : Int
+    @State var selectedItems : String
+    
+    
+    var body: some View {
+        Button {
+            toggleTimer()
+            showMessage = false
+            
+            if timeInterval == 0{
+                timeInterval = timerhour
+                stopTimer()
+                hydrationActivationViewModel.stopPlaying()
+            }
+            
+            if timerhour == 0 && timeInterval == 0 {
+                DispatchQueue.main
+                    .asyncAfter(deadline: .now() + 0.2, execute: {
+                        withAnimation {
+                            showMessage = true
+                        }
+                    })
+            }
+            
+        } label: {
+            ZStack {
+                Circle()
+                    .fill(fill)
+                    .frame(width: 200, height: 200)
+                    .shadow(radius: 10)
+                //                                .scaleEffect(timerIsReading ? 1.05 : 1)
+                //                                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: timerIsReading)
+                
+                Text(buttonLabel)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+            }
+        }
+        .buttonStyle(.plain)
+        .animation(.spring(), value: timerIsReading)
+    }
+    
+    
+    func toggleTimer() {
+        withAnimation {
+            hydrationActivationViewModel.authorization()
+            timerIsReading.toggle()
+            if  timerIsReading {
+                startTimer()
+            }else{
+                stopTimer()
+            }
+        }
+    }
+    
+    var fill : Color {
+        switch buttonLabel{
+        case "Réinitialiser" :
+            return Color("ToReboot")
+        case "REPRENDRE" :
+            return Color("ToResume")
+        case "COMMENCER" :
+            return Color("ToBegin")
+        default:
+            return .gray
+        }
+    }
+    
+    func stopTimer() {
+        cancellable?.cancel()
+        if let start = startDate {
+            let elapsedTime = Int(Date().timeIntervalSince(start))
+            elapseBeforPause += elapsedTime
+        }
+    }
+    
+    var buttonLabel: String {
+        if timeInterval == 0 {
+            return "Réinitialiser"
+        }
+        else {
+            if timerIsReading && timeInterval != 0 && timeInterval != timerhour {
+                return "STOPPER"
+            } else if timeInterval < timerhour {
+                return "REPRENDRE"
+            } else {
+                return "COMMENCER"
+            }
+        }
+    }
+    
+    
+    func startTimer() {
+        startDate = Date()
+        cancellable?.cancel()
+        cancellable = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                guard let start = startDate else { return }
+                let elapsedTime = elapseBeforPause + Int(Date().timeIntervalSince(start))
+                let remainingTime = max(timerhour - elapsedTime, 0)
+                timeInterval = remainingTime
+                
+                if timeInterval == 0 {
+                    stopTimer()
+                    hydrationActivationViewModel.notification()
+                    hydrationActivationViewModel.playingSound(audioFile: selectedItems)
+                    elapseBeforPause = 0
+                }
+            }
+    }
+}
+
+
+#Preview {
+    Start_timer()
+}
