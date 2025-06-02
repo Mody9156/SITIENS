53ac5245cc8b77cf00e1ee8b5567d990e615ef9c
KEITA Modibo <Modykeita2@gmail.com>
2025-06-02 14:41:02 +0200

    initialisation

--- SITIENS/View/MapView.swift
+++ SITIENS/View/MapView.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map{
+            Marker(
+                "Paris",
+                coordinate: CLLocationCoordinate2D(latitude: 48.862725, longitude: 2.287592)
+                
+            )
+            .tint(.orange)
+            Marker("Lyon", coordinate: CLLocationCoordinate2D(latitude: 45.7578137, longitude: 4.8320114))
+                                .tint(.blue)
+            Marker(
+                "Marseille",
+                coordinate: CLLocationCoordinate2D(
+                    latitude:43.2961743,
+                    longitude: 5.3699525
+                )
+            )
+        } .mapControlVisibility(.hidden)
+        
+        
     }
 }
 
