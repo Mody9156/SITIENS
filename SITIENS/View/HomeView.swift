f5493b59aa37d9ec31f9291806f3197a68442d31
KEITA Modibo <Modykeita2@gmail.com>
2025-06-02 11:24:49 +0200

    nouvelle variabel

--- SITIENS/View/HomeView.swift
+++ SITIENS/View/HomeView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding  var activeNavLink : Bool
-    
+    var moreText : String {
+     return   """
+        Il est généralement conseillé de boire entre 1,3 litre et 2 litres d’eau par jour. Tout dépend de son poids et de son niveau d’activité dans la journée. Mais dans tous les cas, si un organisme perd plus de 15 % de son poids en eau, le pronostic vital est alors engagé.
+
+        Tout au long de la journée, notre corps perd en moyenne 2,6 litres d’eau, sous forme de transpiration, urines, respiration, larmes. Et pour compenser cette perte, il est recommandé de compenser cette perte hydrique par la boisson et la nourriture.
+
+        Lorsque nous consommons des aliments riches en eau comme les fruits et les légumes, nous apportons environ 1 litre d’eau à notre organisme.
+        Le métabolisme des nutriments produit quant à lui environ 30 cl d’eau endogène.
+        Il faut donc apporter le reste en eau de boisson.
+        
+        Mais attention à ne pas attendre d’avoir soif pour boire ! Car même si la soif permet de compenser les pertes hydriques de l’organisme, la sensation de soif indique que la déshydratation est déjà amorcée. Il faut donc penser à boire régulièrement au fil de la journée, sachant qu’une quantité de 1,5 litre correspond environ à 8 verres d’eau. L’idéal est de boire souvent et en petites quantités.
+        """
+    }
     var body: some View {
         NavigationStack {
             ZStack{
@@ -54,9 +66,14 @@ struct HomeView: View {
                                 .fontWeight(.semibold)
                                 .multilineTextAlignment(.leading)
                             
-                            Text("Notre organisme est composé de 60 à 65 % d’eau. Et c’est précisément cette eau présente dans notre corps qui permet d’assurer de nombreuses fonctions vitales de notre corps. On comprend alors pourquoi il est si important de boire régulièrement et en quantité suffisante pour maintenir notre corps en bonne santé.")
+                            Text(
+                                 """
+                                 Notre organisme est composé de 60 à 65 % d’eau. Et c’est précisément cette eau présente dans notre corps qui permet d’assurer de nombreuses fonctions vitales de notre corps. On comprend alors pourquoi il est si important de boire régulièrement et en quantité suffisante pour maintenir notre corps en bonne santé.
+                                 """)
                                 .font(.body)
                                 .multilineTextAlignment(.leading)
+                            
+                            
                         }
                         .padding()
                     }
