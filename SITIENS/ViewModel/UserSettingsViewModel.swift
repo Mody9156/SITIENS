67535f444889ccc2ae381d64e96f3e5dbb329b95
KEITA Modibo <Modykeita2@gmail.com>
2025-06-18 18:00:55 +0200

    nouvelle fonction

--- SITIENS/ViewModel/UserSettingsViewModel.swift
+++ SITIENS/ViewModel/UserSettingsViewModel.swift
@@ -67,35 +67,7 @@ class UserSettingsViewModel{
     //indiqué la quantité en fonction du type de verre ⛔️
     //créer un struct avec par exemple quantity && name pour la picture
     //
-    func uptateQuanittyOfWater(name type: String) -> Int{
-        switch type  {
-        case "Nourrissons":
-            return 2
-        case "Femmes enceintes":
-            return 8
-        case "Personnes âgées":
-            return 6
-        case "Sportifs":
-            return 1
-        case "Enfants et adolescents":
-            return 8
-        case "Travailleurs en environnement chaud":
-            return 1
-        case "Personnes souffrant de maladies chroniques":
-            return 6
-        case "Personnes en surpoids ou obèses":
-            return 1
-        case "Voyageurs ou personnes en altitude":
-            return 1
-        case "Personnes sous traitement médicamenteux":
-            return 6
-        default:
-            return 0
-        }
-  
-    }
-
-    func chooseBottleOfWater(name type: String, _ number : Int) -> Int {
+    func uptateQuanittyOfWater(name type: String, _ number :Int) -> Int{
         switch type {
         case "Petit – 200 ml" :
             return number / 15
@@ -106,9 +78,24 @@ class UserSettingsViewModel{
         default :
             return 0
         }
+  
     }
+
+    func chooseBottleOfWater(name type: String) -> String {
+        switch type {
+        case "Petit – 200 ml" :
+            return "SmallGlace"
+        case "Moyen – 300 ml" :
+            return "MediumGlace"
+        case "Grand – 500 ml" :
+            return "LargeGlace"
+        default :
+            return ""
+        }
+    }
+ 
     
     // pour 300 c'est number/300 = 10
-    // pour 200 c'est number/200 = 15
-    // pour 500 c'est number/10 = 6
+    // pour 300 c'est number/10
+    // pour 500 c'est number/10
 }
