0358f7bfd0591e76421a24c37dadf037d6fd8887
KEITA Modibo <Modykeita2@gmail.com>
2025-06-27 15:03:50 +0200

    nouvelle variable

--- SITIENS/View/WaterQuantityView.swift
+++ SITIENS/View/WaterQuantityView.swift
@@ -10,7 +10,14 @@ import CoreData
 import Combine
 
 struct WaterQuantityView: View {
-    @State var updateHeight : CGFloat = 0
+    @AppStorage("updateHeight") var updateHeightRaw : Double = 0
+    
+    var updateHeight : CGFloat {
+        get{updateHeightRaw}
+        set{updateHeightRaw = Double(newValue)}
+        
+    }
+    
     @State var sheetPresented : Bool = false
     @State var rotationInfiny : Bool = false
     @State var profilType : String = ""
