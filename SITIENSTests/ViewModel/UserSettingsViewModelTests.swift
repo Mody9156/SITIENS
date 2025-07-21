ac4cc0c65f1553c2a08ac4e6b961a64c2dd8a4dc
KEITA Modibo <Modykeita2@gmail.com>
2025-07-21 23:36:13 +0200

    modification de la fonction

--- SITIENSTests/ViewModel/UserSettingsViewModelTests.swift
+++ SITIENSTests/ViewModel/UserSettingsViewModelTests.swift
@@ -355,7 +355,7 @@ struct UserSettingsViewModelTests {
             )
         
         //Then
-        #expect(user == "MediumGlace")
+        #expect(user == " MediumGlace")
             
         }
     
