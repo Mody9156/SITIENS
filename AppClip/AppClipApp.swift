0f0384b4459ef1648cb192d11488c04d0b93caab
KEITA Modibo <Modykeita2@gmail.com>
2025-08-01 23:00:57 +0200

    nouvelle appClip

---
+++ AppClip/AppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipApp.swift
+//  AppClip
+//
+//  Created by Modibo on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppClipTests/AppClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipTests.swift
+//  AppClipTests
+//
+//  Created by Modibo on 01/08/2025.
+//
+
+import Testing
+@testable import AppClip
+
+struct AppClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
