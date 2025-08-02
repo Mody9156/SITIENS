e7d77883101acadd8747c90cc990ff1639acdbf0
KEITA Modibo <Modykeita2@gmail.com>
2025-08-02 10:46:57 +0200

    modification du main

--- AppClip/AppClipApp.swift
+++ AppClip/AppClipApp.swift
@@ -11,8 +11,40 @@ import SwiftUI
 struct AppClipApp: App {
     @State private var hasSeenIntro: Bool = false
     var body: some Scene {
+      
         WindowGroup {
-            AppClipHomeView(hasSeenIntro: $hasSeenIntro)
+            Group {
+                if hasSeenIntro {
+                    TabView {
+                       
+                        WaterQuantityViewToAppClip()
+                            .transition(.opacity)
+                            .tabItem {
+                                Label("Hydratation", systemImage: "drop.fill")
+                            }
+
+                        // Tu peux réactiver cette partie si MapView est prêt :
+                        /*
+                        MapView(waterAPIViewModel: WaterAPIViewModel())
+                            .tabItem {
+                                Label("Carte", systemImage: "map")
+                            }
+                        */
+                    }
+                } else {
+                    TabView {
+                        AppClipHomeView(hasSeenIntro: $hasSeenIntro)
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .always))
+                    .background(Color.black)
+                    .ignoresSafeArea()
+                    .onAppear {
+                        UIPageControl.appearance().pageIndicatorTintColor = .gray
+                        UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(named: "background") ?? .white
+                    }
+                }
+            }
+            .animation(.easeOut, value: hasSeenIntro)
         }
     }
 }
---
+++ AppClip/View/WaterQuantityViewToAppClip.swift
@@ -0,0 +1,18 @@
+//
+//  WaterQuantityViewToAppClip.swift
+//  AppClip
+//
+//  Created by Modibo on 02/08/2025.
+//
+
+import SwiftUI
+
+struct WaterQuantityViewToAppClip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaterQuantityViewToAppClip()
+}
