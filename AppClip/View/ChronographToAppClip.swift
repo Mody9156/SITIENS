3fb9104b2a06d9a25d5ea9cf0f1b7bfda2bdcf3b
KEITA Modibo <Modykeita2@gmail.com>
2025-08-02 17:18:03 +0200

    nouvelle vue

--- AppClip/View/ChronographToAppClip.swift
+++ AppClip/View/ChronographToAppClip.swift
@@ -8,9 +8,160 @@
 import SwiftUI
 
 struct ChronographToAppClip: View {
+    @State var timerIsReading = false
+   
+    @State var timeInterval: Int = 0
+    @AppStorage("timeInterval") var timeIntervalRaw: Int = 0
+    @State var startDate: Date?
+    @AppStorage("startDate") var startDateRaw: Date?
+    @State var sheetPresented: Bool = false
+    @State var rotationInfiny: Bool = false
+    @State var selectedItems: String = ""
+    @AppStorage("selectedItems") var selectedItemsRaw: String = ""
+    @AppStorage("hour") var timerhour: Int = 0
+    @State var showMessage: Bool = false
+    @AppStorage("showMessage") var showMessageRaw: Bool = false
+    @State var elapseBeforPause: Int = 0
+    @AppStorage("elapseBeforPause") var elapseBeforPauseRaw: Int = 0
+    @AppStorage("buttonLabel") var buttonLabel: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                
+                // MARK: - Fond avec dégradé bleu/cyan
+                LinearGradient(
+                    gradient: Gradient(colors: [.blue.opacity(0.3), .cyan.opacity(0.2)]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+                
+                VStack(spacing: 32) {
+                    VStack(spacing: 4) {
+                        Text("Hydratation")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundStyle(.primary)
+                            .accessibilityAddTraits(.isHeader)
+                            .accessibilityLabel("Titre: Hydradation")
+                        
+                        Text("Temps restant")
+                            .font(.title3)
+                            .foregroundStyle(.secondary)
+                            .accessibilityLabel("Indication : Temps restant")
+                    }
+                    
+                    ZStack {
+                        
+                        Circle()
+                            .stroke(Color.gray.opacity(0.2), lineWidth: 15)
+                            .frame(height: 300)
+                            .overlay {
+                                Circle()
+                                    .trim(from: 0,to: progressWater())
+                                    .stroke(
+                                        style: StrokeStyle(
+                                            lineWidth: 15,
+                                            lineCap: .round,
+                                            lineJoin: .round,
+                                        )
+                                    )
+                                    .foregroundStyle(completed() ? .blue : .orange)
+                                    .animation(
+                                        .easeInOut(duration: 0.5),
+                                        value: progressWater()
+                                    )
+                            }
+                        
+                        Text("00:00")
+                            .font(.system(size: 48, weight: .bold, design: .monospaced))
+                            .foregroundStyle(.primary)
+                          
+                        
+                    }
+                    .accessibilityElement(children: .combine)
+                    .accessibilityLabel("Progression de l'hydratation")
+                    // MARK: - Bouton du cercle principal
+                    HStack {
+                    //
+                    }
+                    
+                    if showMessage {
+                        HStack {
+                            Image(systemName: "exclamationmark.triangle.fill")
+                                .foregroundStyle(Color.orange)
+                            Text("Veuillez bien selectionner l'horraire")
+                        }
+                        .opacity(showMessage ? 1 : 0)
+                        .animation(
+                            .easeOut(duration: 1.0),value: showMessage
+                        )
+                        .padding()
+                        .accessibilityLabel("Avertissement")
+                        .accessibilityHint("Veuillez bien sélectionner l’horaire")
+                    }
+                }
+                .toolbar(
+                    content: {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                withAnimation {
+                                    sheetPresented = true
+                                }
+                            } label: {
+                                Image(systemName: "gearshape.fill")
+                                    .font(.title2)
+                                    .foregroundStyle(.primary)
+                                    .rotationEffect(.degrees(rotationInfiny ? 360 : 0))
+                                    .animation(
+                                        .linear(duration: 2.9)
+                                        .repeatForever(autoreverses: false),
+                                        value: rotationInfiny
+                                    )
+                                    .onAppear{
+                                        rotationInfiny = true
+                                    }
+                            }
+                            .accessibilityLabel("Bouton des réglages")
+                            .accessibilityHint("Appuyez pour modifier les paramètres du minuteur")
+                            
+                            .disabled(timerIsReading && timeInterval != 0 && timeInterval != timerhour)
+                            .sheet(isPresented: $sheetPresented) {
+                                
+                            } content: {
+                               
+                            }
+                        }
+                    })
+            }
+            .onAppear {
+                timeInterval = timeIntervalRaw
+                selectedItems = selectedItemsRaw
+                showMessage = false
+                startDate = startDateRaw
+                elapseBeforPause = elapseBeforPauseRaw
+                
+                print("elapseBeforPause après le appear -> \(elapseBeforPause)")
+            }
+            .onChange(of: timeInterval) {
+                timeIntervalRaw = timeInterval
+                selectedItemsRaw = selectedItems
+                showMessageRaw = showMessage
+                startDateRaw = startDate
+                elapseBeforPauseRaw = elapseBeforPause
+            }
+        }
     }
+    
+    func completed() -> Bool {
+        return progressWater() == 1
+    }
+    
+    func progressWater() -> CGFloat {
+        return  CGFloat(timeInterval) / CGFloat(timerhour)
+    }
+
 }
 
 #Preview {
