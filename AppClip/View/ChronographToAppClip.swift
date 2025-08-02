6ba53e015923b43e3fb16311505723c7bd9e188c
KEITA Modibo <Modykeita2@gmail.com>
2025-08-02 14:10:41 +0200

    nouvelle vue

---
+++ AppClip/View/ChronographToAppClip.swift
@@ -0,0 +1,18 @@
+//
+//  ChronographToAppClip.swift
+//  AppClip
+//
+//  Created by Modibo on 02/08/2025.
+//
+
+import SwiftUI
+
+struct ChronographToAppClip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChronographToAppClip()
+}
