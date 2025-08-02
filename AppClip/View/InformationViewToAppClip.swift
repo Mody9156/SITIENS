7e7a440f1221f782310a6e4a011c4341a56d2c3f
KEITA Modibo <Modykeita2@gmail.com>
2025-08-02 18:19:53 +0200

    nouvelle vue

---
+++ AppClip/View/InformationViewToAppClip.swift
@@ -0,0 +1,18 @@
+//
+//  InformationViewToAppClip.swift
+//  AppClip
+//
+//  Created by Modibo on 02/08/2025.
+//
+
+import SwiftUI
+
+struct InformationViewToAppClip: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    InformationViewToAppClip()
+}
