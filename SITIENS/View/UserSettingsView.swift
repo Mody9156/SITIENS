4c5003ac1ee049a81c69ae2cc5f8edb150303d32
KEITA Modibo <Modykeita2@gmail.com>
2025-09-08 15:47:49 +0200

    nouvelle extraction

--- SITIENS/View/UserSettingsView.swift
+++ SITIENS/View/UserSettingsView.swift
@@ -15,45 +15,48 @@ struct UserSettingsView: View {
     @Binding var glace : String
     @Environment(\.dismiss) var dismiss
     
+    
+    
     var body: some View {
-        VStack {
-            List {
-                Picker("Profile", selection: $profil) {
-                    ForEach(profileType,id: \.self) { profile in
-                        Text(profile)
+        NavigationStack {
+            VStack {
+                List {
+                    Picker("Profile", selection: $profil) {
+                        ForEach(profileType,id: \.self) { profile in
+                            Text(profile)
+                                .lineLimit(1)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.gray)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                    CustomPicker(glace: $glace, type: $sizeOfGlace)
+                    
+                }
+                
+                Button {
+                    withAnimation {
+                        if !profil.isEmpty && !glace.isEmpty{
+                            dismiss()
+                        }
+                    }
+                    
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .frame(height: 50)
+                        Text("Valider")
+                            .foregroundStyle(.white)
                     }
                 }
-                .pickerStyle(.inline)
-                
-                Picker("Type de récipient", selection: $glace) {
-                    ForEach(sizeOfGlace,id: \.self) { profile in
-                        Text(profile)
-                    }
-                }
-                .pickerStyle(.inline)
-                
+                .disabled(profil.isEmpty)
+                .padding()
+                .accessibilityLabel("Bouton Valider")
+                .accessibilityHint("Valide vos préférences de profil et de récipient")
+                .accessibilityAddTraits(.isButton)
             }
-            
-            Button {
-                withAnimation {
-                    if !profil.isEmpty && !glace.isEmpty{
-                        dismiss()
-                    }
-                }
-                
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(height: 50)
-                    Text("Valider")
-                        .foregroundStyle(.white)
-                }
-            }
-            .disabled(profil.isEmpty)
-            .padding()
-            .accessibilityLabel("Bouton Valider")
-            .accessibilityHint("Valide vos préférences de profil et de récipient")
-            .accessibilityAddTraits(.isButton)
         }
     }
 }
@@ -62,4 +65,21 @@ struct UserSettingsView: View {
     @Previewable @State var profilType : String = ""
     @Previewable @State var glace : String = ""
     UserSettingsView(profil: $profilType, glace: $glace)
+}
+
+struct CustomPicker: View {
+    @Binding var glace : String
+    @Binding var type : [String]
+    var body: some View {
+        Picker("Type de récipient", selection: $glace) {
+            ForEach(type,id: \.self) { profile in
+                Text(profile)
+                    .lineLimit(1)
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
 }
