b16353c2e5c2f216290ac2484c8010c0ab58c874
KEITA Modibo <Modykeita2@gmail.com>
2025-09-27 13:28:34 +0200

    modification d'un test

--- SITIENSUITests/SITIENSUITestsLaunchTests.swift
+++ SITIENSUITests/SITIENSUITestsLaunchTests.swift
@@ -1,33 +1,33 @@
+////
+////  SITIENSUITestsLaunchTests.swift
+////  SITIENSUITests
+////
+////  Created by Modibo on 07/04/2025.
+////
 //
-//  SITIENSUITestsLaunchTests.swift
-//  SITIENSUITests
+//import XCTest
 //
-//  Created by Modibo on 07/04/2025.
+//final class SITIENSUITestsLaunchTests: XCTestCase {
 //
-
-import XCTest
-
-final class SITIENSUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    @MainActor
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
-}
+//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+//        true
+//    }
+//
+//    override func setUpWithError() throws {
+//        continueAfterFailure = false
+//    }
+//
+//    @MainActor
+//    func testLaunch() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Insert steps here to perform after app launch but before taking a screenshot,
+//        // such as logging into a test account or navigating somewhere in the app
+//
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+//    }
+//}
