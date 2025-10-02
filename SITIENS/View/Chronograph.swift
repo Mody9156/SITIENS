18b51b6821a8410074dcfc935c76c7dd929e8d73
KEITA Modibo <Modykeita2@gmail.com>
2025-10-02 15:47:11 +0200

    nouvelle ViewBuilder

--- SITIENS/View/Chronograph.swift
+++ SITIENS/View/Chronograph.swift
@@ -31,6 +31,7 @@ struct Chronograph: View {
     @AppStorage("elapseBeforPause") var elapseBeforPauseRaw: Int = 0
     @AppStorage("buttonLabel") var buttonLabel: String = ""
     @Environment(\.verticalSizeClass) var verticalSizeClass
+    
     var body: some View {
         NavigationStack {
             ZStack {
@@ -43,133 +44,15 @@ struct Chronograph: View {
                 )
                 .ignoresSafeArea()
                 
-                VStack(spacing: 32) {
-                    VStack(spacing: 4) {
-                        Text("Hydratation")
-                            .font(.system(.largeTitle, design: .rounded))
-                            .fontWeight(.bold)
-                            .foregroundStyle(.primary)
-                            .accessibilityAddTraits(.isHeader)
-                            .accessibilityLabel("Titre: Hydradation")
-                        
-                        Text("Temps restant")
-                            .font(.title3)
-                            .foregroundStyle(.secondary)
-                            .accessibilityLabel("Indication : Temps restant")
+                if verticalSizeClass == .compact {
+                    ScrollView {
+                        container()
                     }
-                    
-                    ZStack {
-                        
-                        Circle()
-                            .stroke(Color.gray.opacity(0.2), lineWidth: 15)
-                            .frame(height: 300)
-                            .overlay {
-                                Circle()
-                                    .trim(from: 0,to: progressWater())
-                                    .stroke(
-                                        style: StrokeStyle(
-                                            lineWidth: 15,
-                                            lineCap: .round,
-                                            lineJoin: .round,
-                                        )
-                                    )
-                                    .foregroundStyle(completed() ? .blue : .orange)
-                                    .animation(
-                                        .easeInOut(duration: 0.5),
-                                        value: progressWater()
-                                    )
-                            }
-                     
-                            Text(hydrationActivationViewModel.formatTimer(timeInterval))
-                                .font(.system(size: 48, weight: .bold, design: .monospaced))
-                                .foregroundStyle(.primary)
-                                .accessibilityLabel("Temps restant")
-                                .accessibilityValue(hydrationActivationViewModel.formatTimer(timeInterval))
-                        
-                    }
-                    .accessibilityElement(children: .combine)
-                    .accessibilityLabel("Progression de l'hydratation")
-                    // MARK: - Bouton du cercle principal
-                    HStack {
-                        Start_timer(
-                            showMessage: $showMessage, hydrationActivationViewModel: hydrationActivationViewModel,
-                            timeInterval: $timeInterval,
-                            timerIsReading: $timerIsReading,
-                            cancellable: $cancellable,
-                            startDate: $startDate,
-                            elapseBeforPause: $elapseBeforPause,
-                            selectedItems: $selectedItems,
-                            nameBtm: "Start"
-                        )
-                        .disabled(selectedItems.isEmpty)
-                        .accessibilityLabel("Bouton démarrer le minuteur")
-                        
-                        Start_timer(
-                            showMessage: $showMessage, hydrationActivationViewModel: hydrationActivationViewModel,
-                            timeInterval: $timeInterval,
-                            timerIsReading: $timerIsReading,
-                            cancellable: $cancellable,
-                            startDate: $startDate,
-                            elapseBeforPause: $elapseBeforPause,
-                            selectedItems: $selectedItems,
-                            nameBtm: "stop"
-                        )
-                        .disabled(selectedItems.isEmpty)
-                        .accessibilityLabel("Bouton arrêter le minuteur")
-                    }
-                    
-                    if showMessage {
-                        HStack {
-                            Image(systemName: "exclamationmark.triangle.fill")
-                                .foregroundStyle(Color.orange)
-                            Text("Veuillez bien selectionner l'horraire")
-                        }
-                        .opacity(showMessage ? 1 : 0)
-                        .animation(
-                            .easeOut(duration: 1.0),value: showMessage
-                        )
-                        .padding()
-                        .accessibilityLabel("Avertissement")
-                        .accessibilityHint("Veuillez bien sélectionner l’horaire")
-                    }
+                }else {
+                    container()
+                       
                 }
-                .toolbar(
-                    content: {
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button {
-                                withAnimation {
-                                    sheetPresented = true
-                                }
-                            } label: {
-                                Image(systemName: "gearshape.fill")
-                                    .font(.title2)
-                                    .foregroundStyle(.primary)
-                                    // Fix the layout by giving the symbol a constant square frame
-                                    .frame(width: 32, height: 32, alignment: .center)
-                                    .contentShape(Rectangle())
-                                    .rotationEffect(.degrees(rotationInfiny ? 360 : 0))
-                                    .animation(
-                                        .linear(duration: 1.9)
-                                            .repeatForever(autoreverses: false),
-                                        value: rotationInfiny
-                                    )
-                                    .onAppear {
-                                        rotationInfiny = true
-                                    }
-                            }
-                            .accessibilityLabel("Bouton des réglages")
-                            .accessibilityHint("Appuyez pour modifier les paramètres du minuteur")
-                            .disabled(timerIsReading && timeInterval != 0 && timeInterval != timerhour)
-                            .sheet(isPresented: $sheetPresented) {
-                                
-                            } content: {
-                                TimerSettings(
-                                    selectedItems: $selectedItems,
-                                    selectedHour: $timeInterval, hydrationActivationViewModel: hydrationActivationViewModel
-                                )
-                            }
-                        }
-                    })
+                
             }
             .onAppear {
 //                UserDefaults.standard.removeObject(forKey: "hour")
@@ -189,7 +72,7 @@ struct Chronograph: View {
                 elapseBeforPauseRaw = elapseBeforPause
             }
         }
-        .environment(hydrationActivationViewModel.self)
+        
     }
     
     func completed() -> Bool {
@@ -198,6 +81,139 @@ struct Chronograph: View {
     
     func progressWater() -> CGFloat {
         return  CGFloat(timeInterval) / CGFloat(timerhour)
+    }
+    
+    @ViewBuilder
+    func container() -> some View {
+        VStack(spacing: 32) {
+            VStack(spacing: 4) {
+                Text("Hydratation")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundStyle(.primary)
+                    .accessibilityAddTraits(.isHeader)
+                    .accessibilityLabel("Titre: Hydradation")
+                
+                Text("Temps restant")
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+                    .accessibilityLabel("Indication : Temps restant")
+            }
+            
+            ZStack {
+                
+                Circle()
+                    .stroke(Color.gray.opacity(0.2), lineWidth: 15)
+                    .frame(height: 300)
+                    .overlay {
+                        Circle()
+                            .trim(from: 0,to: progressWater())
+                            .stroke(
+                                style: StrokeStyle(
+                                    lineWidth: 15,
+                                    lineCap: .round,
+                                    lineJoin: .round,
+                                )
+                            )
+                            .foregroundStyle(completed() ? .blue : .orange)
+                            .animation(
+                                .easeInOut(duration: 0.5),
+                                value: progressWater()
+                            )
+                    }
+             
+                    Text(hydrationActivationViewModel.formatTimer(timeInterval))
+                        .font(.system(size: 48, weight: .bold, design: .monospaced))
+                        .foregroundStyle(.primary)
+                        .accessibilityLabel("Temps restant")
+                        .accessibilityValue(hydrationActivationViewModel.formatTimer(timeInterval))
+                
+            }
+            .accessibilityElement(children: .combine)
+            .accessibilityLabel("Progression de l'hydratation")
+            // MARK: - Bouton du cercle principal
+            HStack {
+                Start_timer(
+                    showMessage: $showMessage, hydrationActivationViewModel: hydrationActivationViewModel,
+                    timeInterval: $timeInterval,
+                    timerIsReading: $timerIsReading,
+                    cancellable: $cancellable,
+                    startDate: $startDate,
+                    elapseBeforPause: $elapseBeforPause,
+                    selectedItems: $selectedItems,
+                    nameBtm: "Start"
+                )
+                .disabled(selectedItems.isEmpty)
+                .accessibilityLabel("Bouton démarrer le minuteur")
+                
+                Start_timer(
+                    showMessage: $showMessage, hydrationActivationViewModel: hydrationActivationViewModel,
+                    timeInterval: $timeInterval,
+                    timerIsReading: $timerIsReading,
+                    cancellable: $cancellable,
+                    startDate: $startDate,
+                    elapseBeforPause: $elapseBeforPause,
+                    selectedItems: $selectedItems,
+                    nameBtm: "stop"
+                )
+                .disabled(selectedItems.isEmpty)
+                .accessibilityLabel("Bouton arrêter le minuteur")
+            }
+            
+            if showMessage {
+                HStack {
+                    Image(systemName: "exclamationmark.triangle.fill")
+                        .foregroundStyle(Color.orange)
+                    Text("Veuillez bien selectionner l'horraire")
+                }
+                .opacity(showMessage ? 1 : 0)
+                .animation(
+                    .easeOut(duration: 1.0),value: showMessage
+                )
+                .padding()
+                .accessibilityLabel("Avertissement")
+                .accessibilityHint("Veuillez bien sélectionner l’horaire")
+            }
+        }
+    }
+    
+    @ToolbarContent
+    func toolbarContent() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+                withAnimation {
+                    sheetPresented = true
+                }
+            } label: {
+                Image(systemName: "gearshape.fill")
+                    .font(.title2)
+                    .foregroundStyle(.primary)
+                    // Fix the layout by giving the symbol a constant square frame
+                    .frame(width: 32, height: 32, alignment: .center)
+                    .contentShape(Rectangle())
+                    .rotationEffect(.degrees(rotationInfiny ? 360 : 0))
+                    .animation(
+                        .linear(duration: 1.9)
+                            .repeatForever(autoreverses: false),
+                        value: rotationInfiny
+                    )
+                    .onAppear {
+                        rotationInfiny = true
+                    }
+            }
+            .accessibilityLabel("Bouton des réglages")
+            .accessibilityHint("Appuyez pour modifier les paramètres du minuteur")
+            .disabled(timerIsReading && timeInterval != 0 && timeInterval != timerhour)
+            .sheet(isPresented: $sheetPresented) {
+                
+            } content: {
+                TimerSettings(
+                    selectedItems: $selectedItems,
+                    selectedHour: $timeInterval, hydrationActivationViewModel: hydrationActivationViewModel
+                )
+            }
+        }
+    
     }
 }
 
