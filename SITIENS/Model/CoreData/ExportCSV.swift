5311c2308f7b2bc072cf6b5987b774a3632a4da2
KEITA Modibo <Modykeita2@gmail.com>
2026-05-03 00:03:42 +0200

    nouvelle constante

--- SITIENS/Model/CoreData/ExportCSV.swift
+++ SITIENS/Model/CoreData/ExportCSV.swift
@@ -12,12 +12,14 @@ struct ExportCSV {
     
     func exportCSV(context:NSManagedObjectContext){
         let request = NSFetchRequest<NSManagedObject>(entityName: "History")
+        var minutesSinceLAstDrink : Int = 0
         
         do{
             let result = try context.fetch(request)
-            guard  !result.isEmpty else {
+            
+            guard !result.isEmpty else {
                 print("données vide")
-            return
+                return
             }
             
             let formatter = DateFormatter()
@@ -32,14 +34,13 @@ struct ExportCSV {
                 let date = results.value(forKey: "date") as? String ?? ""
                 
                 
-                guard let realDate = formatter.date(from: date)else {
+                guard let realDate = formatter.date(from: date) else {
                     print("Mauvaise date")
                     continue // si date valdie passe à l'entrée suivante
                 }
                 
                 let entry = ManagementHistory(name: name, quantity: quantity, date: date)
                 entries.append(entry)
-
                 
             }
             
@@ -52,20 +53,28 @@ struct ExportCSV {
             for (index,entry) in sorted.enumerated() {
                 guard let entryDay = formatter.date(from: entry.date) else {continue}
                 
-                let day = Calendar.current.component(.day, from: entryDay)
+                let hour = Calendar.current.component(.hour, from: entryDay)
                 let dayOfWeek = Calendar.current.component(
                     .weekday,
                     from: entryDay
                 ) - 1
+                
+                if index == 0 {
+                    minutesSinceLAstDrink = 120
+                }else {
+                    let previousEntry = sorted[index - 1]
+                    guard let previousEntryDate = formatter.date(from: previousEntry.date) else { continue }
+                    let diff = entryDay.timeIntervalSince(previousEntryDate) / 60
+                    minutesSinceLatDrink = Int(diff)
+                    
                 }
-            
-            
+                
+            }
             
         }catch{
             
             print("❌ Erreur fetch : \(error)")
         }
-        
         
     }
 }
